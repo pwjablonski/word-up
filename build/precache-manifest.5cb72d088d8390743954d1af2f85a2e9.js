ed3d6e310fc5752bb1df8dfc6ec7bffd83f92e23
Peter Jablonski <pwjablonski@gmail.com>
2019-07-29 14:22:06 -0400

    add firebase

---
+++ build/precache-manifest.5cb72d088d8390743954d1af2f85a2e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3b5c9113138e1de058b97fed607a1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a17260f9a4d6a37a590",
+    "url": "/static/css/main.2f90a610.chunk.css"
+  },
+  {
+    "revision": "67cec246ff32e790e374",
+    "url": "/static/js/2.c207cbe6.chunk.js"
+  },
+  {
+    "revision": "4a17260f9a4d6a37a590",
+    "url": "/static/js/main.9d04ae9a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
