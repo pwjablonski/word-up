7e30f69e0f590dee7288b43cf2ee192a1ab827d3
Peter Jablonski <pwjablonski@gmail.com>
2019-08-08 16:16:41 -0400

    update env variables

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
+const API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
+const PROJECT_ID = process.env.REACT_APP_FIREBASE_PROJECT_ID;
+
 module.exports = {
-  firebaseApp: "word-up-dev",
-  firebaseApiKey: "AIzaSyDcAuSl_SBbe1cseKixY1HTlWIaD5HtqzA"
+  firebaseApp: PROJECT_ID,
+  firebaseApiKey: API_KEY
 };
