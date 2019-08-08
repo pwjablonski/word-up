a49accf77e074735fb4d4fc81f7bac6f7ffc20e7
Peter Jablonski <pwjablonski@gmail.com>
2019-08-08 14:48:54 -0400

    update logo

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -3,11 +3,14 @@ import "../css/App.css";
 import DownloadMenu from "./DownloadMenu";
 import UserMenu from "./UserMenu";
 import ToolsMenu from "./ToolsMenu";
+import Wordmark from "../static/wordmark.svg";
 
 export default function Topbar() {
   return (
     <div className="topbar">
-      <div className="topbar-brand">WU</div>
+      <div className="topbar-brand">
+        <img className="topbar-wordmark" src={Wordmark} alt="logo" />
+      </div>
       <div className="topbar-tools">
         <DownloadMenu />
         <ToolsMenu />
